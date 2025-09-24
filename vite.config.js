3f4bbd2ac53e5f18f4f91cbf7fba79b12f327ac4
SINDHUBABU <sindhubabs2@gmail.com>
2025-09-24 08:03:02 +0530

    v1.2.0

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  base: process.env.VITE_BASE || '/',
+}))
